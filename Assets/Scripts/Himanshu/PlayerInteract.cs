f2be318214e8538fa178cac6e066e22f3a5192d8
himchawla <58020143+himchawla@users.noreply.github.com>
2021-09-06 14:56:18 +1200

    HidingTest

--- Assets/Scripts/Himanshu/PlayerInteract.cs
+++ Assets/Scripts/Himanshu/PlayerInteract.cs
@@ -12,10 +12,14 @@ namespace Himanshu
 
         public int bulletCount => m_bulletCount;
         public bool m_hiding;
-        private PlayerInput m_playerInput;
+        public PlayerInput m_playerInput;
         private RaycastingTesting m_raycastingTesting;
+        private HidingSpot m_hidingSpot;
+        private PlayerFollow m_playerFollow;
+        
         private void Start()
         {
+            m_playerFollow = GameObject.FindObjectOfType<PlayerFollow>();
             m_raycastingTesting = FindObjectOfType<RaycastingTesting>();
             m_playerInput = GetComponent<PlayerInput>();
         }
@@ -26,9 +30,9 @@ namespace Himanshu
             {
                 m_raycastingTesting.ObjectInFront?.GetComponent<IInteract>()?.Execute(this);
             }
-            else
+            else if(m_playerInput.interact)
             {
-                m_hiding = false;
+                Unhide();
             }
 
             if (m_playerInput.interact)
@@ -36,8 +40,18 @@ namespace Himanshu
                 m_raycastingTesting.ObjectInFront?.GetComponent<IEnemy>()?.Shoot(this);
             }
 
-            if(Input.GetKeyDown(KeyCode.A))
+            if(Input.GetKeyDown(KeyCode.B))
                 StartCoroutine(TimeHandler());
+        }
+
+        private void Unhide()
+        {
+            transform.Translate(transform.forward);
+            GetComponent<CharacterController>().enabled = true;
+            m_playerFollow.ResetRotationLock();
+            m_hidingSpot.Disable();
+            m_hiding = false;
+            m_hidingSpot = null;
         }
 
         private IEnumerator TimeHandler()
@@ -53,9 +67,20 @@ namespace Himanshu
             }
         }
 
-        public void Hide()
+        public void Hide(HidingSpot _hidingSpot)
         {
+            m_hidingSpot = _hidingSpot;
+            GetComponent<CharacterController>().enabled = false;
             Debug.Log("Hiding now");
+            m_hiding = true;
+        }
+
+        public void SetPositionAndRotation(Transform _transform)
+        {
+            GetComponent<CharacterController>().enabled = false;
+            transform.position = _transform.position;
+            m_playerFollow.SetRotation(_transform, new Vector2(-30, 30));
+            GetComponent<CharacterController>().enabled = true;
         }
 
         public void Collect()
